1283acc27cb5b174f54daecd2405e0e551cbff27
liuxxxxxxxxx <1038510224@qq.com>
2019-01-29 00:12:42 +0800

    thymeleaf

--- microservice-travels-consumer/src/main/java/com/ljj/springcloud/web/testAction.java
+++ microservice-travels-consumer/src/main/java/com/ljj/springcloud/web/testAction.java
@@ -1,12 +1,15 @@
 package com.ljj.springcloud.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class testAction {
     @GetMapping("/test")
-    public String test(){
-        return "hello";
+    public String test(Model model){
+        model.addAttribute("hello","hhhhhhh");
+        return "test";
     }
 }
